6beba87104958c5b027e1b417ce1e8eb7a07bce7
Sedaca <82716720+Sedaca@users.noreply.github.com>
2021-04-19 18:34:07 -0300

    Exercicio5

---
+++ Exercicio5.py
@@ -0,0 +1,14 @@
+
+5+3*2
+5**2
+9**2
+81**(1/2)
+14//2
+14%2
+nome = input('Qual o seu nome?')
+print('Prazer em te conhecer, {}!'.format(nome))
+print('Prazer em te conhecer, {:30}!'.format(nome))
+print('Prazer em te conhecer, {:>30}!'.format(nome))
+print('Prazer em te conhecer, {:<30}!'.format(nome))
+print('Prazer em te conhecer, {:^30}!'.format(nome))
+print('Prazer em te conhecer, {:-^30}!'.format(nome))
