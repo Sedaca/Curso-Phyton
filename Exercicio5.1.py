7f822fb3d574d99b3ee3e97cfc2a37ffb62f3e9a
Sedaca <82716720+Sedaca@users.noreply.github.com>
2021-04-19 18:33:52 -0300

    Exercicio5.1

---
+++ Exercicio5.1.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1+n2
+sub = n1-n2
+m = n1*n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+
+print('O número antecessor de {} é {}'.format(n1,(n1-1)))
+print('O número sucessor de {} é {}'.format(n1,(n1+1)))
